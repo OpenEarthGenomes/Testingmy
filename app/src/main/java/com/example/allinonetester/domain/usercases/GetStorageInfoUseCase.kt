afa29b0acae8b7fc17c97bf2d8eccec7acaa1872
Fáber Sándor <sandorwow@gmail.com>
2026-06-14 14:17:22 +0200

    Update GetStorageInfoUseCase.kt

--- app/src/main/java/com/example/allinonetester/domain/usercases/GetStorageInfoUseCase.kt
+++ app/src/main/java/com/example/allinonetester/domain/usercases/GetStorageInfoUseCase.kt
@@ -1,1 +1,9 @@
+package com.example.allinonetester.domain.usercases
 
+import com.example.allinonetester.utils.DeviceUtils
+
+class GetStorageInfoUseCase {
+    suspend operator fun invoke(): String {
+        return DeviceUtils.getStorageInfo()
+    }
+}
