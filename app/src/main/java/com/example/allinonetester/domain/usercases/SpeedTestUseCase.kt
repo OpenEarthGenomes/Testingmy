52d0fc75cc4fb9512f32b078b57c50a66d77c013
Fáber Sándor <sandorwow@gmail.com>
2026-06-14 14:13:38 +0200

    Update SpeedTestUseCase.kt

--- app/src/main/java/com/example/allinonetester/domain/usercases/SpeedTestUseCase.kt
+++ app/src/main/java/com/example/allinonetester/domain/usercases/SpeedTestUseCase.kt
@@ -1,1 +1,14 @@
+package com.example.allinonetester.domain.usercases
 
+import com.example.allinonetester.utils.NetworkUtils
+
+class SpeedTestUseCase {
+    suspend operator fun invoke(): String {
+        return try {
+            val speedMbps = NetworkUtils.simpleDownloadSpeed()
+            "Letöltési sebesség: %.2f Mbps 🚀".format(speedMbps)
+        } catch (e: Exception) {
+            "Sebességteszt hiba: ${e.message}"
+        }
+    }
+}
