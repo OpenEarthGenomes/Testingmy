c5a30c3873c0a04810f2e09f21b78783e5b0d1d2
Fáber Sándor <sandorwow@gmail.com>
2026-06-14 14:16:54 +0200

    Implement GetRamInfoUseCase to retrieve RAM info

--- app/src/main/java/com/example/allinonetester/domain/usercases/GetRamInfoUseCase.kt
+++ app/src/main/java/com/example/allinonetester/domain/usercases/GetRamInfoUseCase.kt
@@ -1,1 +1,9 @@
+package com.example.allinonetester.domain.usercases
 
+import com.example.allinonetester.utils.DeviceUtils
+
+class GetRamInfoUseCase {
+    suspend operator fun invoke(): String {
+        return DeviceUtils.getRamInfo()
+    }
+}
