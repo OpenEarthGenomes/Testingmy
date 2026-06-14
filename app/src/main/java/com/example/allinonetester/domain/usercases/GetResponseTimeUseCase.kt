d9a5d54d03ad8271b1882267dcf78835211fd112
Fáber Sándor <sandorwow@gmail.com>
2026-06-14 14:08:10 +0200

    Implement GetResponseTimeUseCase to fetch response time

--- app/src/main/java/com/example/allinonetester/domain/usercases/GetResponseTimeUseCase.kt
+++ app/src/main/java/com/example/allinonetester/domain/usercases/GetResponseTimeUseCase.kt
@@ -1,1 +1,14 @@
+package com.example.allinonetester.domain.usercases
 
+import com.example.allinonetester.utils.NetworkUtils
+
+class GetResponseTimeUseCase {
+    suspend operator fun invoke(url: String = "https://clcoding.com"): String {
+        return try {
+            val (status, timeMs) = NetworkUtils.getResponseTime(url)
+            "URL: $url\nStatus: $status | Válaszidő: ${timeMs} ms ⏱️"
+        } catch (e: Exception) {
+            "Hiba: ${e.message}"
+        }
+    }
+}
