aa9f1a50b8771e8d55d42cb5ef9c9262ebd1b477
Fáber Sándor <sandorwow@gmail.com>
2026-06-14 14:29:24 +0200

    Update GetMobileNetworkTypeUseCase.kt

--- app/src/main/java/com/example/allinonetester/domain/usercases/GetMobileNetworkTypeUseCase.kt
+++ app/src/main/java/com/example/allinonetester/domain/usercases/GetMobileNetworkTypeUseCase.kt
@@ -1,1 +1,10 @@
+package com.example.allinonetester.domain.usercases
 
+import android.content.Context
+import com.example.allinonetester.utils.DeviceUtils
+
+class GetMobileNetworkTypeUseCase(private val context: Context) {
+    suspend operator fun invoke(): String {
+        return "Mobil hálózat típusa: ${DeviceUtils.getMobileNetworkType(context)} 📶"
+    }
+}
