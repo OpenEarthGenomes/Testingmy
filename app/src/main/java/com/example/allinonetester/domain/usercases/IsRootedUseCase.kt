31829af2ec55e8a83bae2061a21991e59d3b3d24
Fáber Sándor <sandorwow@gmail.com>
2026-06-14 14:31:44 +0200

    Update IsRootedUseCase.kt

--- app/src/main/java/com/example/allinonetester/domain/usercases/IsRootedUseCase.kt
+++ app/src/main/java/com/example/allinonetester/domain/usercases/IsRootedUseCase.kt
@@ -1,1 +1,9 @@
+package com.example.allinonetester.domain.usercases
 
+import com.example.allinonetester.utils.DeviceUtils
+
+class IsRootedUseCase {
+    suspend operator fun invoke(): String {
+        return "Root hozzáférés: ${if (DeviceUtils.isRooted()) "IGEN ⚠️" else "NEM ✅"}"
+    }
+}
