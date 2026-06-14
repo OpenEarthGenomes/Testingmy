41bd3f8c314a30a0caddffe280594a655641f662
Fáber Sándor <sandorwow@gmail.com>
2026-06-14 14:05:52 +0200

    Update DnsLookupUseCase.kt

--- app/src/main/java/com/example/allinonetester/domain/usercases/DnsLookupUseCase.kt
+++ app/src/main/java/com/example/allinonetester/domain/usercases/DnsLookupUseCase.kt
@@ -1,1 +1,14 @@
+package com.example.allinonetester.domain.usercases
 
+import com.example.allinonetester.utils.NetworkUtils
+
+class DnsLookupUseCase {
+    suspend operator fun invoke(domain: String = "clcoding.com"): String {
+        return try {
+            val ip = NetworkUtils.dnsLookup(domain)
+            "Domain: $domain → IP: $ip"
+        } catch (e: Exception) {
+            "Hiba: ${e.message}"
+        }
+    }
+}
