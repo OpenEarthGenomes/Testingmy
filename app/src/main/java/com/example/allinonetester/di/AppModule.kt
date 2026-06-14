17a59f80407c10a0d2d7fc91568f5820445db6fb
Fáber Sándor <sandorwow@gmail.com>
2026-06-14 13:31:18 +0200

    Update AppModule.kt

--- app/src/main/java/com/example/allinonetester/di/AppModule.kt
+++ app/src/main/java/com/example/allinonetester/di/AppModule.kt
@@ -1,1 +1,15 @@
+package com.example.allinonetester.di
 
+import com.example.allinonetester.utils.CacheManager
+import com.example.allinonetester.utils.RateLimiter
+import com.example.allinonetester.utils.TokenCounter
+
+object AppModule {
+    private val cacheManager = CacheManager()
+    private val rateLimiter = RateLimiter()
+    private val tokenCounter = TokenCounter()
+    
+    fun provideCacheManager(): CacheManager = cacheManager
+    fun provideRateLimiter(): RateLimiter = rateLimiter
+    fun provideTokenCounter(): TokenCounter = tokenCounter
+}
