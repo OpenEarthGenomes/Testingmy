b67da0bd47b1c15fe318e64c6c8e3f3baa83b294
Fáber Sándor <sandorwow@gmail.com>
2026-06-14 14:15:59 +0200

    Update GetBatteryStatusUseCase.kt

--- app/src/main/java/com/example/allinonetester/domain/usercases/GetBatteryStatusUseCase.kt
+++ app/src/main/java/com/example/allinonetester/domain/usercases/GetBatteryStatusUseCase.kt
@@ -1,1 +1,10 @@
+package com.example.allinonetester.domain.usercases
 
+import android.content.Context
+import com.example.allinonetester.utils.DeviceUtils
+
+class GetBatteryStatusUseCase(private val context: Context) {
+    suspend operator fun invoke(): String {
+        return DeviceUtils.getBatteryInfo(context)
+    }
+}
