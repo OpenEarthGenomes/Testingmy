4ebcdff40e6231627a4657addce70af1af7d8574
Fáber Sándor <sandorwow@gmail.com>
2026-06-14 14:33:56 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/allinonetester/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/allinonetester/presentation/ui/MainActivity.kt
@@ -1,1 +1,32 @@
+package com.example.allinonetester.presentation.ui
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.allinonetester.presentation.theme.AllInOneTesterTheme
+import com.example.allinonetester.presentation.ui.screens.MainScreen
+import com.example.allinonetester.presentation.ui.viewmodel.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AllInOneTesterTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val viewModel: MainViewModel = viewModel()
+                    MainScreen(viewModel = viewModel)
+                }
+            }
+        }
+    }
+}
