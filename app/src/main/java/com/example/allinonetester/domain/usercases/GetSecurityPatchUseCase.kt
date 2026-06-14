c4d0998afd663bd760754fcad30ec2b914d1cd96
Fáber Sándor <sandorwow@gmail.com>
2026-06-14 14:31:06 +0200

    Update GetSecurityPatchUseCase.kt

--- app/src/main/java/com/example/allinonetester/domain/usercases/GetSecurityPatchUseCase.kt
+++ app/src/main/java/com/example/allinonetester/domain/usercases/GetSecurityPatchUseCase.kt
@@ -1,1 +1,9 @@
+package com.example.allinonetester.domain.usercases
 
+import com.example.allinonetester.utils.DeviceUtils
+
+class GetSecurityPatchUseCase {
+    suspend operator fun invoke(): String {
+        return DeviceUtils.getSecurityPatch()
+    }
+}
