d57d64d39d821312a9097f7fe7ba9ca42ee08cca
Fáber Sándor <sandorwow@gmail.com>
2026-06-14 19:04:07 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,5 @@
-// Top-level build file
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.hilt.android) apply false
-    alias(libs.plugins.kotlin.kapt) apply false
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false
 }
