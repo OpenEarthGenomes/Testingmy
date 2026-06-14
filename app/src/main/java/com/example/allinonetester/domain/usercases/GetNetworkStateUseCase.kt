08028f440742b66b45551949148c25e1393f2e9f
Fáber Sándor <sandorwow@gmail.com>
2026-06-14 14:16:28 +0200

    Update GetNetworkStateUseCase.kt

--- app/src/main/java/com/example/allinonetester/domain/usercases/GetNetworkStateUseCase.kt
+++ app/src/main/java/com/example/allinonetester/domain/usercases/GetNetworkStateUseCase.kt
@@ -1,1 +1,10 @@
+package com.example.allinonetester.domain.usercases
 
+import android.content.Context
+import com.example.allinonetester.utils.DeviceUtils
+
+class GetNetworkStateUseCase(private val context: Context) {
+    suspend operator fun invoke(): String {
+        return DeviceUtils.getNetworkState(context)
+    }
+}
