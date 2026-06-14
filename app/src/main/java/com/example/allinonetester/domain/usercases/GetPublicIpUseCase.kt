c5dbac583eff8f693c4daf27f56236bbd2375c73
Fáber Sándor <sandorwow@gmail.com>
2026-06-14 14:06:43 +0200

    Implement GetPublicIpUseCase to fetch public IP

--- app/src/main/java/com/example/allinonetester/domain/usercases/GetPublicIpUseCase.kt
+++ app/src/main/java/com/example/allinonetester/domain/usercases/GetPublicIpUseCase.kt
@@ -1,1 +1,14 @@
+package com.example.allinonetester.domain.usercases
 
+import com.example.allinonetester.utils.NetworkUtils
+
+class GetPublicIpUseCase {
+    suspend operator fun invoke(): String {
+        return try {
+            val ip = NetworkUtils.getPublicIp()
+            "Publikus IP cím: $ip 🌐"
+        } catch (e: Exception) {
+            "Hiba: ${e.message}"
+        }
+    }
+}
