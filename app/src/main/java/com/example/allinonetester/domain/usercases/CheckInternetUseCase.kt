0ba64c11619ad0b155bd6e44dd243283a0c6ec57
Fáber Sándor <sandorwow@gmail.com>
2026-06-14 14:04:35 +0200

    Update CheckInternetUseCase.kt

--- app/src/main/java/com/example/allinonetester/domain/usercases/CheckInternetUseCase.kt
+++ app/src/main/java/com/example/allinonetester/domain/usercases/CheckInternetUseCase.kt
@@ -1,1 +1,15 @@
+package com.example.allinonetester.domain.usercases
 
+import android.content.Context
+import com.example.allinonetester.utils.NetworkUtils
+
+class CheckInternetUseCase(private val context: Context) {
+    suspend operator fun invoke(): String {
+        return try {
+            val isConnected = NetworkUtils.isInternetAvailable(context)
+            "Internet elérhető: ${if (isConnected) "IGEN ✅" else "NEM ❌"}"
+        } catch (e: Exception) {
+            "Hiba: ${e.message}"
+        }
+    }
+}
