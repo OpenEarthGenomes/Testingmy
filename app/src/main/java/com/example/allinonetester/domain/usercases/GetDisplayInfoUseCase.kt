d74edbc3cf8a81a57d49077e5c4a9cd34078584c
Fáber Sándor <sandorwow@gmail.com>
2026-06-14 14:30:37 +0200

    Update GetDisplayInfoUseCase.kt

--- app/src/main/java/com/example/allinonetester/domain/usercases/GetDisplayInfoUseCase.kt
+++ app/src/main/java/com/example/allinonetester/domain/usercases/GetDisplayInfoUseCase.kt
@@ -1,1 +1,10 @@
+package com.example.allinonetester.domain.usercases
 
+import android.content.Context
+import com.example.allinonetester.utils.DeviceUtils
+
+class GetDisplayInfoUseCase(private val context: Context) {
+    suspend operator fun invoke(): String {
+        return DeviceUtils.getDisplayInfo(context)
+    }
+}
