2510337c977506cc03fbf928daa3350f8ad21155
Fáber Sándor <sandorwow@gmail.com>
2026-06-14 14:39:02 +0200

    Implement Logger utility for logging messages

--- app/src/main/java/com/example/allinonetester/utils/Logger.kt
+++ app/src/main/java/com/example/allinonetester/utils/Logger.kt
@@ -1,1 +1,21 @@
+package com.example.allinonetester.utils
 
+import android.util.Log
+import com.example.allinonetester.config.AppConfig
+
+object Logger {
+    private const val TAG = "AllInOneTester"
+    private var isDebug = true
+    
+    fun d(message: String) {
+        if (isDebug) Log.d(TAG, message)
+    }
+    
+    fun e(message: String, throwable: Throwable? = null) {
+        Log.e(TAG, message, throwable)
+    }
+    
+    fun i(message: String) {
+        Log.i(TAG, message)
+    }
+}
