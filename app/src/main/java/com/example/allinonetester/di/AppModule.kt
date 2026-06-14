561f5bd239348bfa7be340101e120e1a387c7b2b
Fáber Sándor <sandorwow@gmail.com>
2026-06-14 17:02:14 +0200

    Update AppModule.kt

--- app/src/main/java/com/example/allinonetester/di/AppModule.kt
+++ app/src/main/java/com/example/allinonetester/di/AppModule.kt
@@ -1,10 +1,9 @@
 package com.example.allinonetester.di
 
 import android.content.Context
-import com.example.allinonetester.domain.usercases.*
-import com.example.allinonetester.utils.CacheManager
-import com.example.allinonetester.utils.RateLimiter
-import com.example.allinonetester.utils.TokenCounter
+import com.example.allinonetester.domain.usercases.ClearRamUseCase
+import com.example.allinonetester.domain.usercases.FileOperationsUseCase
+import com.example.allinonetester.domain.usercases.GetFolderSizeUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -18,79 +17,25 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideCacheManager(@ApplicationContext context: Context): CacheManager = CacheManager(context)
+    fun provideContext(@ApplicationContext context: Context): Context {
+        return context
+    }
 
     @Provides
     @Singleton
-    fun provideRateLimiter(): RateLimiter = RateLimiter()
+    fun provideGetFolderSizeUseCase(context: Context): GetFolderSizeUseCase {
+        return GetFolderSizeUseCase(context)
+    }
 
     @Provides
     @Singleton
-    fun provideTokenCounter(): TokenCounter = TokenCounter()
-
-    // Itt a Context-et igénylő UseCase-ek Factory-jai
-    @Provides
-    fun provideCheckInternetUseCase(@ApplicationContext context: Context) = CheckInternetUseCase(context)
+    fun provideClearRamUseCase(context: Context): ClearRamUseCase {
+        return ClearRamUseCase(context)
+    }
 
     @Provides
-    fun provideGetBatteryStatusUseCase(@ApplicationContext context: Context) = GetBatteryStatusUseCase(context)
-
-    @Provides
-    fun provideGetNetworkStateUseCase(@ApplicationContext context: Context) = GetNetworkStateUseCase(context)
-
-    @Provides
-    fun provideGetScreenBrightnessUseCase(@ApplicationContext context: Context) = GetScreenBrightnessUseCase(context)
-
-    @Provides
-    fun provideListInstalledAppsUseCase(@ApplicationContext context: Context) = ListInstalledAppsUseCase(context)
-
-    @Provides
-    fun provideGetMobileNetworkTypeUseCase(@ApplicationContext context: Context) = GetMobileNetworkTypeUseCase(context)
-
-    @Provides
-    fun provideListSensorsUseCase(@ApplicationContext context: Context) = ListSensorsUseCase(context)
-
-    @Provides
-    fun provideGetDisplayInfoUseCase(@ApplicationContext context: Context) = GetDisplayInfoUseCase(context)
-    
-    @Provides
-    fun provideGetFolderSizeUseCase(@ApplicationContext context: Context) = GetFolderSizeUseCase(context)
-
-    // A Context nélküli UseCase-ek
-    @Provides
-    fun provideDnsLookupUseCase() = DnsLookupUseCase()
-
-    @Provides
-    fun provideGetPublicIpUseCase() = GetPublicIpUseCase()
-
-    @Provides
-    fun provideGetResponseTimeUseCase() = GetResponseTimeUseCase()
-
-    @Provides
-    fun provideSpeedTestUseCase() = SpeedTestUseCase()
-
-    @Provides
-    fun providePingUseCase() = PingUseCase()
-
-    @Provides
-    fun providePortScanUseCase() = PortScanUseCase()
-
-    @Provides
-    fun provideGetRamInfoUseCase() = GetRamInfoUseCase()
-
-    @Provides
-    fun provideGetStorageInfoUseCase() = GetStorageInfoUseCase()
-
-    @Provides
-    fun provideGetDeviceInfoUseCase() = GetDeviceInfoUseCase()
-
-    @Provides
-    fun provideGetCpuCoreCountUseCase() = GetCpuCoreCountUseCase()
-
-    @Provides
-    fun provideGetSecurityPatchUseCase() = GetSecurityPatchUseCase()
-
-    @Provides
-    fun provideIsRootedUseCase() = IsRootedUseCase()
+    @Singleton
+    fun provideFileOperationsUseCase(context: Context): FileOperationsUseCase {
+        return FileOperationsUseCase(context)
+    }
 }
-
