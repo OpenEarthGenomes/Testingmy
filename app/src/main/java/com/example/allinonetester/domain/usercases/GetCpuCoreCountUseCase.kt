cc45aff81bd93298e2c6bc1b31512603cd068bd4
Fáber Sándor <sandorwow@gmail.com>
2026-06-14 14:28:26 +0200

    Update GetCpuCoreCountUseCase.kt

--- app/src/main/java/com/example/allinonetester/domain/usercases/GetCpuCoreCountUseCase.kt
+++ app/src/main/java/com/example/allinonetester/domain/usercases/GetCpuCoreCountUseCase.kt
@@ -1,1 +1,9 @@
+package com.example.allinonetester.domain.usercases
 
+import com.example.allinonetester.utils.DeviceUtils
+
+class GetCpuCoreCountUseCase {
+    suspend operator fun invoke(): String {
+        return "CPU magok száma: ${DeviceUtils.getCpuCoreCount()} 🧠"
+    }
+}
