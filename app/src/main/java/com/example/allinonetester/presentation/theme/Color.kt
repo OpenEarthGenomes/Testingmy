041119a61d747e97078ee4477e3c21db7e4611fd
Fáber Sándor <sandorwow@gmail.com>
2026-06-14 14:32:23 +0200

    Update Color.kt

--- app/src/main/java/com/example/allinonetester/presentation/theme/Color.kt
+++ app/src/main/java/com/example/allinonetester/presentation/theme/Color.kt
@@ -1,1 +1,22 @@
+package com.example.allinonetester.presentation.theme
 
+import androidx.compose.ui.graphics.Color
+
+// Modern One UI 8 inspired colors
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// Custom colors for Samsung A35 optimization
+val SamsungBlue = Color(0xFF4A90E2)
+val SamsungDarkBg = Color(0xFF121212)
+val SamsungCardBg = Color(0xFF1E1E1E)
+val SamsungTextPrimary = Color(0xFFE0E0E0)
+val SamsungTextSecondary = Color(0xFF9E9E9E)
+val SamsungSuccess = Color(0xFF4CAF50)
+val SamsungError = Color(0xFFF44336)
+val SamsungWarning = Color(0xFFFF9800)
