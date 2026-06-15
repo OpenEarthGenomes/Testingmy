3340abd1a475b775c9420c3c2db9d301932b6def
Fáber Sándor <sandorwow@gmail.com>
2026-06-15 09:54:11 +0200

    Update CheckInternetUseCase.kt

--- app/src/main/java/com/example/allinonetester/domain/usercases/CheckInternetUseCase.kt
+++ app/src/main/java/com/example/allinonetester/domain/usercases/CheckInternetUseCase.kt
@@ -2,9 +2,13 @@ package com.example.allinonetester.domain.usercases
 
 import android.content.Context
 import com.example.allinonetester.utils.NetworkUtils
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
 
-class CheckInternetUseCase(private val context: Context) {
-    suspend operator fun invoke(): String {
+class CheckInternetUseCase @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
+    operator fun invoke(): String {
         return try {
             val isConnected = NetworkUtils.isInternetAvailable(context)
             "Internet elérhető: ${if (isConnected) "IGEN ✅" else "NEM ❌"}"
