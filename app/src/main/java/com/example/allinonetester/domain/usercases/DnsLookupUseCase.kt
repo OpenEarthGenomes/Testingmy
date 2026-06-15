087e83c7c7a36713348128ef4f9b1f5d2b752bbe
Fáber Sándor <sandorwow@gmail.com>
2026-06-15 17:32:30 +0200

    Update DnsLookupUseCase.kt

--- app/src/main/java/com/example/allinonetester/domain/usercases/DnsLookupUseCase.kt
+++ app/src/main/java/com/example/allinonetester/domain/usercases/DnsLookupUseCase.kt
@@ -1,10 +1,13 @@
 package com.example.allinonetester.domain.usercases
 
 import com.example.allinonetester.utils.NetworkUtils
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class DnsLookupUseCase {
-    suspend operator fun invoke(domain: String = "clcoding.com"): String {
-        return try {
+class DnsLookupUseCase @Inject constructor() {
+    suspend operator fun invoke(domain: String = "clcoding.com"): String = withContext(Dispatchers.IO) {
+        try {
             val ip = NetworkUtils.dnsLookup(domain)
             "Domain: $domain → IP: $ip"
         } catch (e: Exception) {
