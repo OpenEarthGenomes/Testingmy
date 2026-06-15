344f7a6b0c6fab6bc497570ff2ad3857a42abc9f
Fáber Sándor <sandorwow@gmail.com>
2026-06-15 17:35:42 +0200

    Update SpeedTestUseCase.kt

--- app/src/main/java/com/example/allinonetester/domain/usercases/SpeedTestUseCase.kt
+++ app/src/main/java/com/example/allinonetester/domain/usercases/SpeedTestUseCase.kt
@@ -1,10 +1,13 @@
 package com.example.allinonetester.domain.usercases
 
 import com.example.allinonetester.utils.NetworkUtils
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class SpeedTestUseCase {
-    suspend operator fun invoke(): String {
-        return try {
+class SpeedTestUseCase @Inject constructor() {
+    suspend operator fun invoke(): String = withContext(Dispatchers.IO) {
+        try {
             val speedMbps = NetworkUtils.simpleDownloadSpeed()
             "Letöltési sebesség: %.2f Mbps 🚀".format(speedMbps)
         } catch (e: Exception) {
