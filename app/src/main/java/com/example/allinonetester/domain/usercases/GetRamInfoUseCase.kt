603ec1a7d7b3409aca3659dc2cadf045e0b0525d
Fáber Sándor <sandorwow@gmail.com>
2026-06-15 09:53:31 +0200

    Update GetRamInfoUseCase.kt

--- app/src/main/java/com/example/allinonetester/domain/usercases/GetRamInfoUseCase.kt
+++ app/src/main/java/com/example/allinonetester/domain/usercases/GetRamInfoUseCase.kt
@@ -1,9 +1,14 @@
 package com.example.allinonetester.domain.usercases
 
+import android.content.Context
 import com.example.allinonetester.utils.DeviceUtils
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
 
-class GetRamInfoUseCase {
-    suspend operator fun invoke(): String {
-        return DeviceUtils.getRamInfo()
+class GetRamInfoUseCase @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
+    operator fun invoke(): String {
+        return DeviceUtils.getRamInfo(context)
     }
 }
