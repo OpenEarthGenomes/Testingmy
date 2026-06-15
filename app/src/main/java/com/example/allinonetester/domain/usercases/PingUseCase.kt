2f99168762e8a3d5a02dfb3b7b016539f7e5ef01
Fáber Sándor <sandorwow@gmail.com>
2026-06-15 17:36:26 +0200

    Refactor PingUseCase to use dependency injection

--- app/src/main/java/com/example/allinonetester/domain/usercases/PingUseCase.kt
+++ app/src/main/java/com/example/allinonetester/domain/usercases/PingUseCase.kt
@@ -1,10 +1,13 @@
 package com.example.allinonetester.domain.usercases
 
 import com.example.allinonetester.utils.NetworkUtils
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class PingUseCase {
-    suspend operator fun invoke(host: String = "1.1.1.1"): String {
-        return try {
+class PingUseCase @Inject constructor() {
+    suspend operator fun invoke(host: String = "1.1.1.1"): String = withContext(Dispatchers.IO) {
+        try {
             val result = NetworkUtils.ping(host)
             "Ping $host:\n$result 📡"
         } catch (e: Exception) {
