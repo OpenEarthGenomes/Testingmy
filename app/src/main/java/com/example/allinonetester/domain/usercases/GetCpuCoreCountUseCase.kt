b6a26629da71a04e1902cecb99efd8dc7964794a
Fáber Sándor <sandorwow@gmail.com>
2026-06-15 17:39:59 +0200

    Update GetCpuCoreCountUseCase.kt

--- app/src/main/java/com/example/allinonetester/domain/usercases/GetCpuCoreCountUseCase.kt
+++ app/src/main/java/com/example/allinonetester/domain/usercases/GetCpuCoreCountUseCase.kt
@@ -1,8 +1,9 @@
 package com.example.allinonetester.domain.usercases
 
 import com.example.allinonetester.utils.DeviceUtils
+import javax.inject.Inject
 
-class GetCpuCoreCountUseCase {
+class GetCpuCoreCountUseCase @Inject constructor() {
     suspend operator fun invoke(): String {
         return "CPU magok száma: ${DeviceUtils.getCpuCoreCount()} 🧠"
     }
