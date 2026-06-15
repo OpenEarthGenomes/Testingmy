7ad6de4ed41596ccbfa5059f7656a12aed618a5e
Fáber Sándor <sandorwow@gmail.com>
2026-06-15 17:35:10 +0200

    Update GetResponseTimeUseCase.kt

--- app/src/main/java/com/example/allinonetester/domain/usercases/GetResponseTimeUseCase.kt
+++ app/src/main/java/com/example/allinonetester/domain/usercases/GetResponseTimeUseCase.kt
@@ -1,10 +1,13 @@
 package com.example.allinonetester.domain.usercases
 
 import com.example.allinonetester.utils.NetworkUtils
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class GetResponseTimeUseCase {
-    suspend operator fun invoke(url: String = "https://clcoding.com"): String {
-        return try {
+class GetResponseTimeUseCase @Inject constructor() {
+    suspend operator fun invoke(url: String = "https://clcoding.com"): String = withContext(Dispatchers.IO) {
+        try {
             val timeMs = NetworkUtils.getResponseTime(url)
             "URL: $url\nVálaszidő: $timeMs ⏱️"
         } catch (e: Exception) {
