a8ca52dd90f76557354d0a0fa8ea374b049b3fd6
Fáber Sándor <sandorwow@gmail.com>
2026-06-15 08:55:01 +0200

    Update GetMobileNetworkTypeUseCase.kt

--- app/src/main/java/com/example/allinonetester/domain/usercases/GetMobileNetworkTypeUseCase.kt
+++ app/src/main/java/com/example/allinonetester/domain/usercases/GetMobileNetworkTypeUseCase.kt
@@ -2,9 +2,13 @@ package com.example.allinonetester.domain.usercases
 
 import android.content.Context
 import com.example.allinonetester.utils.DeviceUtils
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
 
-class GetMobileNetworkTypeUseCase(private val context: Context) {
-    suspend operator fun invoke(): String {
-        return "Mobil hálózat típusa: ${DeviceUtils.getMobileNetworkType(context)} 📶"
+class GetMobileNetworkTypeUseCase @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
+    operator fun invoke(): String {
+        return DeviceUtils.getNetworkState(context)
     }
 }
