299858515866dc6ccd7a2b175dee17a0abc9c229
Fáber Sándor <sandorwow@gmail.com>
2026-06-15 17:41:44 +0200

    Update IsRootedUseCase.kt

--- app/src/main/java/com/example/allinonetester/domain/usercases/IsRootedUseCase.kt
+++ app/src/main/java/com/example/allinonetester/domain/usercases/IsRootedUseCase.kt
@@ -1,8 +1,9 @@
 package com.example.allinonetester.domain.usercases
 
 import com.example.allinonetester.utils.DeviceUtils
+import javax.inject.Inject
 
-class IsRootedUseCase {
+class IsRootedUseCase @Inject constructor() {
     suspend operator fun invoke(): String {
         return "Root hozzáférés: ${if (DeviceUtils.isRooted()) "IGEN ⚠️" else "NEM ✅"}"
     }
