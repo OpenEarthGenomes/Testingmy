d5d1c1b50e0e016e309409158b01706b11213b8d
Fáber Sándor <sandorwow@gmail.com>
2026-06-15 17:39:28 +0200

    Update GetScreenBrightnessUseCase.kt

--- app/src/main/java/com/example/allinonetester/domain/usercases/GetScreenBrightnessUseCase.kt
+++ app/src/main/java/com/example/allinonetester/domain/usercases/GetScreenBrightnessUseCase.kt
@@ -2,8 +2,12 @@ package com.example.allinonetester.domain.usercases
 
 import android.content.Context
 import com.example.allinonetester.utils.DeviceUtils
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
 
-class GetScreenBrightnessUseCase(private val context: Context) {
+class GetScreenBrightnessUseCase @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
     suspend operator fun invoke(): String {
         val brightness = DeviceUtils.getScreenBrightness(context)
         return "Képernyő fényerő: $brightness / 255 ☀️"
