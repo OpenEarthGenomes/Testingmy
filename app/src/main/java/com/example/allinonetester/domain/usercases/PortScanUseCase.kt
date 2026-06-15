91bc6be2a0cdac6d65165584b4063ad6d98147ee
Fáber Sándor <sandorwow@gmail.com>
2026-06-15 17:37:04 +0200

    Update PortScanUseCase.kt

--- app/src/main/java/com/example/allinonetester/domain/usercases/PortScanUseCase.kt
+++ app/src/main/java/com/example/allinonetester/domain/usercases/PortScanUseCase.kt
@@ -1,10 +1,13 @@
 package com.example.allinonetester.domain.usercases
 
 import com.example.allinonetester.utils.NetworkUtils
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class PortScanUseCase {
-    suspend operator fun invoke(host: String = "1.1.1.1", ports: List<Int> = listOf(80, 443, 53, 22, 8080)): String {
-        return try {
+class PortScanUseCase @Inject constructor() {
+    suspend operator fun invoke(host: String = "1.1.1.1", ports: List<Int> = listOf(80, 443, 53, 22, 8080)): String = withContext(Dispatchers.IO) {
+        try {
             val open = NetworkUtils.scanPorts(host, ports)
             if (open.isEmpty()) {
                 "Nincsenek nyitott portok a listából: $host 🔒"
