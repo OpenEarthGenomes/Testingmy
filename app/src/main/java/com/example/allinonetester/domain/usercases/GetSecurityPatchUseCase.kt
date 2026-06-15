d60103508546f79013f09d35ae016034d1c02b16
Fáber Sándor <sandorwow@gmail.com>
2026-06-15 17:41:13 +0200

    Update GetSecurityPatchUseCase.kt

--- app/src/main/java/com/example/allinonetester/domain/usercases/GetSecurityPatchUseCase.kt
+++ app/src/main/java/com/example/allinonetester/domain/usercases/GetSecurityPatchUseCase.kt
@@ -1,8 +1,9 @@
 package com.example.allinonetester.domain.usercases
 
 import com.example.allinonetester.utils.DeviceUtils
+import javax.inject.Inject
 
-class GetSecurityPatchUseCase {
+class GetSecurityPatchUseCase @Inject constructor() {
     suspend operator fun invoke(): String {
         return DeviceUtils.getSecurityPatch()
     }
